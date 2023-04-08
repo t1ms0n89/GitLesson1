2846ecf8fc80cff15b61f55d46a01094a7f6ab50
t1ms0n89 <timur.yedigeyev@gmail.com>
2023-04-09 02:10:09 +0600

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainmethodshowmessageAble();
+        mainMethodShowIndo();
+    }
+    private static void mainmethodshowmessageAble() {
+        System.out.println("Hello able QA course\n");
+    }
+    private static void mainMethodShowIndo() {
+        System.out.println("Lets learn Git");
+    }
+}
